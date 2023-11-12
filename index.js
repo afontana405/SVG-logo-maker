775261f0100e480589f248326d8fa7438687ebbd
afontana405 <alex13579@icloud.com>
2023-11-12 17:16:05 -0600

    added square and triangle functions

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const iquirer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+const {Circle, Square, Triangle} = require('../lib/shape.js');
+const Svg = require('../lib/svg.js');
 
-inquirer.prompr([
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like to use?',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
     {
         type: 'input',
-        name: 'backgroundColor',
+        name: 'shapeColor',
         message: 'What color should the background be?',
     },
     {
@@ -12,4 +20,25 @@ inquirer.prompr([
         name: 'text',
         message: 'Please enter an acronym of three or less characters?',
     },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color should the text be?',
+    },
 ])
+.then(answer => {
+    let newShape;
+    if (answer.shape === 'Circle') {
+        newShape = new Circle();
+    }  else if (answer.shape === 'Square') {
+        newShape = new Square();
+    } else if (answer.shape === 'Triangle') {
+        newShape = new Triangle();
+    }
+    newShape.setColor(answer.shapeColor);
+    let newSVG = new Svg();
+    newSVG.changeText(answer.text, answer.textColor);
+    newSVG.changeShape(newShape);
+    console.log(newSVG);
+})
+.catch(err => console.error(err));
