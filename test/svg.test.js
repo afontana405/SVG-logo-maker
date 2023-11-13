843b93a914a5701ac073e472dd855204e878ecc2
afontana405 <alex13579@icloud.com>
2023-11-13 16:38:23 -0600

    test runs successfully

--- test/svg.test.js
+++ test/svg.test.js
@@ -3,9 +3,10 @@ const Svg = require('../lib/svg.js');
 describe('Svg', () => {
     describe('changeText', () => {
         it('should return a div with the users text and color choices', () => {
-            const text = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="green">css</text>';
-            const textChanger = new Svg.changeText('css', 'green');
-            expect(textchanger.toEqual(text));
+            const data = {text: 'css', textColor: 'green', shape: 'square'};
+            const text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="green">css</text>`;
+            const svg = new Svg(data);
+            expect(svg.changeText("css", "green")).toEqual(text);
         })
     });
 })
